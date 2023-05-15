cdf519d51330616fb0613b01d7e85f12271cc169
Yashbhalgamiya <86512736+Yashbhalgamiya@users.noreply.github.com>
2023-05-15 11:37:47 +0530

    Chapter-2

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Catalog"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Yash Bhalgamiya'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //String name='yash';
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Yash Bhalgamiya'),
-          ),
-        ),
-      )
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
     );
   }
 }
