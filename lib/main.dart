dd9847f0ebe4bbf9d928212eaaa7cdbe6f19dbcf
Yashbhalgamiya <86512736+Yashbhalgamiya@users.noreply.github.com>
2023-05-15 14:22:07 +0530

    Chapter-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home.dart';
+import 'package:flutter_catalog/pages/home.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,20 @@ class MyApp extends StatelessWidget {
     //String name='yash';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        //fontFamily: GoogleFonts.lato().fontFamily
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.light
+      ),
+      routes:{
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>HomePage(),//for defaul screen
+        "/login":(context)=>LoginPage()
+      },
     );
   }
 }
